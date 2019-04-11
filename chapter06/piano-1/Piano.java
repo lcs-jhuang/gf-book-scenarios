4de601efd848ad9a32cf9ade42db82abeb3c2015
Joe Huang <jhuang@lcs.on.ca>
2019-04-11 09:44:36 -0400

    got time tracking working

--- chapter06/piano-1/Piano.java
+++ chapter06/piano-1/Piano.java
@@ -9,10 +9,32 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 public class Piano extends World
 {
     /**
+     * Instance Variables
+     * (Can be used anywhere in the class)
+     */
+    int frames;
+    
+     /**
      * Make the piano.
      */
     public Piano() 
     {
         super(800, 340, 1);
+        
+        // initialize instance variables
+        frames = 0;
+    }
+    
+    /**
+     * act
+     * (gets called repeatedly to creat animation)
+     */
+    public void act()
+    {
+        // track frames
+        frames += 1;
+        
+        // show frames
+        showText("" + frames, 100, 100);
     }
 }
