125af60bc95bca4944f1e875b66df94d0665cad5
Joe Huang <jhuang@lcs.on.ca>
2019-04-13 10:35:09 -0400

    Fix bug: sound play

--- chapter06/piano-1/Key.java
+++ chapter06/piano-1/Key.java
@@ -3,10 +3,21 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 public class Key extends Actor
 {
     /**
+     * Instance variables 
+     * Can be used anywhere in the class below
+     */
+    // keep track of whether a key was just presseed
+    private boolean keyAlreadyDown;
+    
+    
+    /**
      * Create a new key.
      */
     public Key()
     {
+        // no key has been pressed
+        keyAlreadyDown = false;
+        
     }
 
     /**
@@ -15,17 +26,24 @@ public class Key extends Actor
     public void act()
     {
         // animate the piano key being presssed
-        if (Greenfoot.isKeyDown("g"))
+        // condition 1 - is the "g" key being pressed
+        // condition 2 - was the "g" key already down
+        if (Greenfoot.isKeyDown("g") && (keyAlreadyDown = false))
         {
             // key is down
             setImage("white-key-down.png");
             play();
+            keyAlreadyDown = true;
         }
         
-        else
+        // stop showing the key being pressed
+        // condition 1 - the key was down the last time act() fired
+        // condition 2 - the "g" has been released no longer being pressed
+        if (keyAlreadyDown == true && Greenfoot.isKeyDown("g") == false)
         {
             // key is up
-            setImage("white-key.png");       
+            setImage("white-key.png");  
+            keyAlreadyDown = false;
         }
         
     }
